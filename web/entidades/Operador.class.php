87620c79785825bcbbab0800326473170603af45
André Jr Dorneles <andre.jr.dorneles@gmail.com>
2018-12-12 04:30:12 -0200

    fix(Operador): corrige entidade

--- web/entidades/Operador.class.php
+++ web/entidades/Operador.class.php
@@ -2,13 +2,13 @@
   class Operador {
     public $id;
     public $nome;
-    public $senha;
+    //public $senha;
     public $login;
     public $url_imagem;
-    private $endereco;
-    private $data_nascimento;
+    // private $endereco;
+    // private $data_nascimento;
 
-    function __construct($id, $nome, $senha, $login, $url_imagem){
+    function __construct($id, $nome, $login, $url_imagem){
       $this->id = $id;
       $this->nome = $nome;
       $this->login = $login;
