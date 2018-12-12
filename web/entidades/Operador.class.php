49254f8614c598324f1195cf593d74b4a0acb368
André Jr Dorneles <andre.jr.dorneles@gmail.com>
2018-12-12 00:08:21 -0200

    fix(login): consulta

--- web/entidades/Operador.class.php
+++ web/entidades/Operador.class.php
@@ -1,16 +1,18 @@
 <?php
   class Operador {
-    private $id;
-    private $nome;
-    private $senha;
-    private $login;
+    public $id;
+    public $nome;
+    public $senha;
+    public $login;
+    public $url_imagem;
     private $endereco;
     private $data_nascimento;
 
-    function __construct($nome, $senha, $login){
+    function __construct($nome, $senha, $login, $url_imagem){
       $this->nome = $nome;
       $this->senha = $senha;
       $this->login = $login;
+      $this->url_imagem = $url_imagem;
     }
   }
 ?>
