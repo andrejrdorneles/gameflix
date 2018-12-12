ea2411efa74da99d9a263e4506b1d15a82d64006
André Jr Dorneles <andre.jr.dorneles@gmail.com>
2018-12-12 00:58:06 -0200

    fix(login): throw error invalid user

--- web/config/connect.php
+++ web/config/connect.php
@@ -4,10 +4,10 @@
   // $db->ssl_set(PATH_TO_SSL_CLIENT_KEY_FILE, PATH_TO_SSL_CLIENT_CERT_FILE, PATH_TO_CA_CERT_FILE, null, null);
   // $db->real_connect(HOSTNAME, USERNAME, PASSWORD, DATABASE_NAME);
 
-  $server = 'us-cdbr-iron-east-01.cleardb.net';
-  $user = 'b7dd97901cca9c';
-  $password = '6591c14d';
-  $database= 'heroku_5627424df39c2ee';
+  $server = 'localhost';
+  $user = 'root';
+  $password = '';
+  $database= 'gameflix';
 
   $mysqli = new mysqli($server, $user, $password, $database);
   
