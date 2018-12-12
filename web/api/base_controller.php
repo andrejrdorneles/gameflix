35c9d952b1e7905770c0ab1e8643cc4546d83e8c
André Jr Dorneles <andre.jr.dorneles@gmail.com>
2018-12-12 02:02:27 -0200

    fix(base_controller): allow methods

--- web/api/base_controller.php
+++ web/api/base_controller.php
@@ -1,6 +1,6 @@
 <?php
   header("Access-Control-Allow-Origin: *");
-  header("Access-Control-Allow-Headers: *");
+  header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS");
 
   $request_method = $_SERVER['REQUEST_METHOD'];
   $postdata = file_get_contents("php://input");
