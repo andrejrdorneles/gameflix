c5e951a3bfe4a91bd3a3811689090633a2e216b5
André Jr Dorneles <andre.jr.dorneles@gmail.com>
2018-12-12 02:22:09 -0200

    fix(base_controller): cors

--- web/api/base_controller.php
+++ web/api/base_controller.php
@@ -1,6 +1,7 @@
 <?php
   header("Access-Control-Allow-Origin: *");
   header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS");
+  header("Access-Control-Allow-Headers: Content-Type");
 
   $request_method = $_SERVER['REQUEST_METHOD'];
   $postdata = file_get_contents("php://input");
